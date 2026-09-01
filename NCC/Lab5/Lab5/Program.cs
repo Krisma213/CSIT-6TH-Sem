8ee7532a82f6aaaed1fe48a74fa2f507010825d3
Krisma213 <krismaa211@gmail.com>
2026-09-01 12:08:05 +0545

    Add files via upload

---
+++ NCC/Lab5/Lab5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+abstract class Employee
+{
+    public string Name;
+
+    public Employee(string name)
+    {
+        Name = name;
+    }
+
+    public abstract double CalculateSalary();
+}
+
+class Manager : Employee
+{
+    public Manager(string name) : base(name)
+    {
+    }
+
+    public override double CalculateSalary()
+    {
+        return 145000 + 10000;
+    }
+}
+
+class Clerk : Employee
+{
+    public Clerk(string name) : base(name)
+    {
+    }
+
+    public override double CalculateSalary()
+    {
+        return 30000 + 3000;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Manager manager = new Manager("Krisma");
+        Clerk clerk = new Clerk("Shrisha");
+
+        Console.WriteLine("Manager Name: " + manager.Name);
+        Console.WriteLine("Manager Salary: " + manager.CalculateSalary());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Clerk Name: " + clerk.Name);
+        Console.WriteLine("Clerk Salary: " + clerk.CalculateSalary());
+    }
+}
